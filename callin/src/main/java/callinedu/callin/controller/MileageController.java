b5e7ad991d231a39dcc039ef007f28d68f41dae6
dlgmlwls0389 <dlgmlwls0389@naver.com>
2021-09-29 13:14:11 +0900

    [callin] 저장버튼 완료

--- callin/src/main/java/callinedu/callin/controller/MileageController.java
+++ callin/src/main/java/callinedu/callin/controller/MileageController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import callinedu.callin.domain.ClassPolicy;
+import callinedu.callin.domain.LTApplyCode;
 import callinedu.callin.domain.Mileage;
 import callinedu.callin.domain.Student;
 import callinedu.callin.domain.Teacher;
@@ -46,30 +48,12 @@ public class MileageController {
 		  model.addAttribute("studentDegreeName",  student.getStudentDegreeName());
 		  model.addAttribute("title", "마일리지");
 		  model.addAttribute("midTitle", "마일리지등록");
-		  
-		  //System.out.println("### map : " + studentService.getTest());
 		 
 	  return  "/mileage/mileageAdd"; 
 	  } 
 	 
 	
-	/**
-	 * 검색
-	 * @param param
-	 * @param request
-	 * @return
-	 * @throws Exception
-	 */
-	/*@RequestMapping(value="mileageSearch")
-	public @ResponseBody Map<String, Object> mileageSearch(@RequestParam Map<String, Object> param, HttpServletRequest request) throws Exception {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		List<Map<String, Object>> resultList = studentService.studentList();
-
-		
-		resultMap.put("resultList", resultList);
-		
-		return resultMap;
-	}*/
+	  //검색
 	@PostMapping(value="/mileageSearch", produces = "application/json")
 	@ResponseBody
 	public List<Student> mileageSearch(@RequestBody Map<String, Object> paramMap) {
@@ -77,14 +61,8 @@ public class MileageController {
 		List<Student> searchList = studentService.getMemberListBySearch(paramMap);
 		return  searchList;
 	}
-	/**
-	 * 저장
-	 * @param param
-	 * @param request
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value="mileageSave")
+	//저장
+	/*@RequestMapping(value="mileageSave")
 	public String mileageSave(@RequestParam Map<String, Object> param, @RequestParam(value="moveSlct", required=false) String[] moveSlcts, HttpServletRequest request, Mileage mileage ) throws Exception {
 		System.out.println("### param : " + param);
 		//여기도 실제 insert문 태우는걸로 
@@ -102,6 +80,19 @@ public class MileageController {
 		}	
 		return "redirect:/admin/maileage/maileageAdd";
 			
+	}*/
+	@PostMapping("addMileage")
+	public String addMileage(Model model, @ModelAttribute Mileage Mileage) {
+		System.out.println("마일리지등록 컨트롤러 실행" + "---" + Mileage.toString());
+		model.addAttribute("title", "마일리지");
+		model.addAttribute("midTitle", "마일리지등록");		
+		
+		int result = studentService.addMileage(Mileage);
+		
+		System.out.println(result);
+		return "redirect:/admin/mileage/mileageAdd";
+		
+		
 	}
 
 
